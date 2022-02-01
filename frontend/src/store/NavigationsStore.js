b623f89f9d557832c7954afcc4ec7ff2f672e9fb
iamsiwatdev <s1104300051612@gmail.com>
2022-02-02 00:31:45 +0700

    Store Persist

--- frontend/src/store/NavigationsStore.js
+++ frontend/src/store/NavigationsStore.js
@@ -1,10 +1,21 @@
 import { createStore } from 'vuex'
 
+/**
+ * Store Persist เป็นการเก็บ store ของ vuex ลงไปที่ localstoreage ทำให้พอ refresh หน้าเว็บแล้วข้อมูลตัวแปรไม่ถูกล้างค่าข้อมูล ข้อมูลในตัวแปร state จะอยู่ใน localstoreage ให้เองโดยอัติโนมัติ
+ */
+import VuexPersistence from 'vuex-persist'
+
+const vuexLocal = new VuexPersistence({ //Setup vuex-persist
+  storage: window.localStorage
+})
+
 // store คือ พื้นที่ส่วนกลางที่ให้ app ของเราดึงค่าไปใช้งานได้
 export default createStore({
+  
   state: { //ส่วนที่เก็บตัวแปรคล้ายกับ data ใน component
-    isAuthen: true
+    isAuthen: false
   },
+
   mutations: { //เป็นตัวกระทำตามคำสั่งของ Actions ในการเพิ่ม ลบ แก้ไขตัวแปร
     //ใช้สำหรับจัดการกับ state พวกเพิ่มค่าให้กับตัวแปร
     SET_AUTHEN (state, payload) {
@@ -12,6 +23,7 @@ export default createStore({
       state.isAuthen = payload
     }
   },
+
   actions: { //ตัวการกระทำเราอยากจะกระทำอะไรแล้วให้เปลี่ยนแปลงค่า เช่น Call API Backend เพิ่ม ลบ แก้ไข คลิ๊กปุ่ม
     //ถ้าเราจะให้ actions ไปเรียกใช้งานใน mutations เราจะต้องใช้คำสั่ง commit
     setAuthen ({ commit }, payload) {
@@ -19,13 +31,15 @@ export default createStore({
       commit('SET_AUTHEN', payload)
     }
   },
+
   getters : { //function ไว้ return value
     isAuthen: (state) => {
       console.log('Store Getters state.isAuthen : ' + state.isAuthen);
       return state.isAuthen //ดึงค่า isAuthen
     }
   },
-  modules: {
 
-  }
+  plugins: [
+    vuexLocal.plugin //เรียกใช้ vuex-persist
+  ]
 })
